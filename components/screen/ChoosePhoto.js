e92e3860f9c08f7b121dc6d9cc09dbd07ec19c61
Claudia Baik <claudiabaik@gmail.com>
2018-03-11 20:16:28 -0400

    added some design changes

--- components/screen/ChoosePhoto.js
+++ components/screen/ChoosePhoto.js
@@ -1,25 +1,64 @@
-import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import React, { Component } from "react";
+import {
+  Button,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native";
+import { StackNavigator } from "react-navigation";
+import EmptyBG from "./EmptyBG";
 
 export default class ChoosePhoto extends React.Component {
-
   render() {
     return (
-      <View>
+      <View style={styles.container}>
+        <EmptyBG source={require("../assets/background.png")} />
 
-        <Button
-          title='Camera to take photo'
-          onPress={() => this.props.navigation.navigate('TakePhoto')}
-        />
+        <TouchableOpacity
+          style={styles.button1}
+          onPress={() => this.props.navigation.navigate("TakePhoto")}
+        >
+          <Text style={styles.text}>   Take a photo   </Text>
+        </TouchableOpacity>
 
-        <Button
-          title='Making a constallation of your photo'
-          onPress={() => this.props.navigation.navigate('PhotoFromLibrary')}
-        />
-
+        <TouchableOpacity
+          style={styles.button2}
+          onPress={() => this.props.navigation.navigate("PhotoFromLibrary")}
+        >
+          <Text style={styles.text}> Choose a photo </Text>
+        </TouchableOpacity>
       </View>
-    )
+    );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button1: {
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+    opacity: 0.5,
+    padding: 10,
+    top: 300,
+    position: "absolute"
+  },
+  button2: {
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+    opacity: 0.5,
+    padding: 10,
+    top: 400,
+    position: "absolute"
+  },
+
+  text: {
+    color: "#3f348c",
+    fontFamily: "Arial",
+    fontSize: 20
+  }
+});
